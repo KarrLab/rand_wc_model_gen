85dfc17ecdb229aa698b39fa34673c226c660af8
Arthur Goldberg <artgoldberg@gmail.com>
2018-01-08 16:10:10 -0500

    add empty class and test it; it appears that circle won't pass without some tests

---
+++ tests/test_core.py
@@ -0,0 +1,19 @@
+""" Test model generation
+
+:Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
+:Date: 2018-01-08
+:Copyright: 2018, Karr Lab
+:License: MIT
+"""
+import unittest
+
+import wc_model_generator
+
+
+class TestGenerateModel(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_test(self):
+        pass
---
+++ wc_model_generator/core.py
@@ -0,0 +1,12 @@
+""" Generate WC models that can be used to test WC tools
+
+:Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
+:Date: 2018-01-08
+:Copyright: 2018, Karr Lab
+:License: MIT
+"""
+
+class GenerateModel(object):
+
+    def __init__(self):
+        pass
