e31a6a9f9039a65a75d2dc80ec3347a86c35917f
Jonathan Karr <jonrkarr@gmail.com>
2018-01-18 01:47:52 -0500

    adding description, keywords, classifiers

--- setup.py
+++ setup.py
@@ -22,14 +22,14 @@ md = pkg_utils.get_package_metadata(dirname, name, package_data_filename_pattern
 setuptools.setup(
     name=name,
     version=md.version,
-    description=name,
+    description='Random whole-cell model generator',
     long_description=md.long_description,
     url="https://github.com/KarrLab/" + name,
     download_url='https://github.com/KarrLab/' + name,
     author="Karr Lab",
     author_email="karr@mssm.com",
     license="MIT",
-    keywords='',
+    keywords='whole-cell systems biology',
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
     package_data=md.package_data,
     install_requires=md.install_requires,
@@ -38,8 +38,10 @@ setuptools.setup(
     dependency_links=md.dependency_links,
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
     ],
     entry_points={
         'console_scripts': [
