649db9f2c09d4201037679921425002e734d9c4d
Cathy Wang <cwcathyshuwang@gmail.com>
2018-01-18 18:55:22 -0500

    create random proteins from DNA instead of RNA; add metabolism data

--- random_wc_model_generator/random_polymer.py
+++ random_wc_model_generator/random_polymer.py
@@ -14,10 +14,11 @@ class RandomSeqGen(object):
     """
 
     # genetic constants
-    RNA_NUCLEOTIDES = ['A','U','C','G']
-    START_CODON = 'AUG'
-    STOP_CODON = 'UAG'
-    NUCLEOTIDE_COMP = {'A':'T','U':'A','C':'G','G':'C'}
+    DNA_NUCLEOTIDES = ['A','T','C','G']
+    # in DNA
+    START_CODON = 'TAC'
+    STOP_CODON = 'ATC'
+    NUCLEOTIDE_COMP = {'A':'U','T':'A','C':'G','G':'C'}
     MIN_PROT_LEN = 100
     MAX_PROT_LEN = 1000
     CODON_TRANS_FILE = os.path.join(os.path.dirname(__file__), 'data/fixtures/codon_translation.csv')
@@ -37,33 +38,48 @@ class RandomSeqGen(object):
             # values = corresponding amino acid, molecular weight, and charge
             self.translation_table[codon] = [amino_acid, float(molecular_weight), int(charge)]
             
-    def prot_seq(self, length):
-        """ Randomly create nucleotide sequence for protein 
+    def make_genome(self, prot_length):
+        """ Make random genome (DNA nucleotide) sequence
 
         Args:
-            length (:obj:`int`): desired length (in amino acids) of protein
+            prot_length (:obj:`list`): list of lengths of each protein
 
         Returns:
-            :obj:`string`: nucleotide sequence of protein
+            :obj:`string`: genome sequence for cell
         """
-        protein = RandomSeqGen.START_CODON
+        genome = ''
+        for i in range(len(prot_length)):
+            genome += RandomSeqGen.START_CODON
+            
+            for j in range(prot_length[i]-2):
+                valid = False
+                while not(valid):
+                    nuc1 = '{}'.format(np.random.choice(RandomSeqGen.DNA_NUCLEOTIDES))
+                    nuc2 = '{}'.format(np.random.choice(RandomSeqGen.DNA_NUCLEOTIDES))
+                    nuc3 = '{}'.format(np.random.choice(RandomSeqGen.DNA_NUCLEOTIDES))
+                    codon = RandomSeqGen.NUCLEOTIDE_COMP[nuc1] + RandomSeqGen.NUCLEOTIDE_COMP[nuc2] + RandomSeqGen.NUCLEOTIDE_COMP[nuc3]
 
-        for i in range(length-2):
-            valid = False
-        
-            while not(valid):
-                nuc1 = '{}'.format(np.random.choice(RandomSeqGen.RNA_NUCLEOTIDES))
-                nuc2 = '{}'.format(np.random.choice(RandomSeqGen.RNA_NUCLEOTIDES))
-                nuc3 = '{}'.format(np.random.choice(RandomSeqGen.RNA_NUCLEOTIDES))
-                codon = nuc1 + nuc2 + nuc3
+                    if codon in self.translation_table.keys():
+                        valid = True
+                genome += nuc1 + nuc2 + nuc3
                 
-                if codon in self.translation_table.keys():
-                    valid = True
-            protein += codon
+            genome += RandomSeqGen.STOP_CODON
+        return genome
     
-        protein += RandomSeqGen.STOP_CODON
+    def rna_transcript(self, genome):
+        """ Complement genome to find mRNA transcript
 
-        return protein
+        Args:
+            genome (:obj:`string`): genome (DNA nucleotide) sequence for cell (includes all genes)
+
+        Returns:
+            :obj:`string`: mRNA nucleotide sequence for cell
+        """
+        rna = ''
+        for nuc in genome:
+            rna += RandomSeqGen.NUCLEOTIDE_COMP[nuc[i]]
+        
+        return rna
     
     def prot_data(self, protein):
         """ Find amino acid sequence, molecular weight, and charge of protein
@@ -95,8 +111,8 @@ class RandomSeqGen(object):
 
         Args:
             num_genes (:obj:`int`): number of genes in cell
-            min_prot_len
-            min_prot_len
+            min_prot_len (:obj:`int`): shortest desired protein length
+            min_prot_len (:obj:`int`): longest desired protein length
 
         Returns:
             :obj:`list`: list of ints for length of each protein
@@ -104,47 +120,59 @@ class RandomSeqGen(object):
         prot_length = np.random.randint(min_prot_len, max_prot_len, num_genes)
         return prot_length
     
-    def make_rna(self, num_genes, prot_len):
+    def make_genes(self, genome):
+        """ Parse genome to create compiled list of DNA nucleotide (gene) sequences for the proteins in cell
+
+        Args:
+            genome (:obj:`string`): genome (DNA nucleotide) sequence for cell (includes all genes)
+
+        Returns:
+            :obj:`list`: list of DNA nucleotide sequences of each protein
+        """
+        genes = ['']
+        codon = ''
+        index = 0
+
+        for j in range(index,len(genome),3):
+            codon = genome[j:j+3]
+            if codon == 'ATC':
+                genes[index] += codon
+                index += 1
+                genes.append('')
+
+            else:
+                genes[index] += codon
+        
+        # get rid of last emtpy string in list
+        genes = genes[:-1]
+    
+        return genes
+
+    def make_mrnas(self, genes):
         """ Create compiled list of RNA nucleotide sequences for the proteins in cell
 
         Args:
-            num_genes (:obj:`int`): number of genes in cell
-            prot_len (:obj:'list'): list of ints for length of each protein
+            genes (:obj:'list'): list of DNA nucleotide sequences of each protein 
 
         Returns:
             :obj:`list`: list of RNA nucleotide sequences of each protein
         """
         prot_rna = []
-        for i in range(num_genes):
-            prot_nucl_seq = self.prot_seq(prot_len[i])
-            prot_rna.append(prot_nucl_seq)
+    
+        for nuc in genes:
+            mrna = ''
+            for i in range(len(nuc)):
+                mrna += RandomSeqGen.NUCLEOTIDE_COMP[nuc[i]]
+            
+            prot_rna.append(mrna)
+    
         return prot_rna
     
-    def make_genes(self, prot_rna):
-        """ Create compiled list of DNA nucleotide (gene) sequences for the proteins in cell
-
-        Args:
-            prot_rna (:obj:`list`): list of RNA nucleotide sequence of each protein
-
-        Returns:
-            :obj:`list`: list of DNA nucleotide sequences of each protein
-        """
-        genes = []
-    
-        for nuc in prot_rna:
-            dna = ''
-            for i in range(len(nuc)):
-                dna += RandomSeqGen.NUCLEOTIDE_COMP[nuc[i]]
-            
-            genes.append(dna)
-    
-        return genes
-
     def make_proteins(self, prot_rna):
         """ Make compiled lists of data (amino acid sequence, molecular weight, and charge) for the proteins in cell
 
         Args:
-            prot_rna (:obj:`list`): list of RNA nucleotide sequences of each protein
+            prot_rna (:obj:`list`): list of mRNA nucleotide sequences of each protein
 
         Returns:
             :obj:`tuple`: lists of amino acid sequences, molecular weights, and charges corresponding to each protein
@@ -172,10 +200,12 @@ class RandomSeqGen(object):
             :obj:`tuple`: lists for DNA, RNA, and amino acid sequence/molecular weight/charge data for each protein
         """
         prot_lengths = self.prot_len(num_genes)
-        rna = self.make_rna(num_genes,prot_lengths)
-        genes = self.make_genes(rna)
-        proteins = self.make_proteins(rna)
-        return (genes, rna, proteins)
+        genome = self.make_genome(prot_lengths)
+        genes = self.make_genes(genome)
+        rnas = self.make_mrnas(genes)
+        proteins = self.make_proteins(rnas)
+        
+        return (genes, rnas, proteins)
 
     def output_sequences(self, proteins, out_file):
         """ Make compiled lists of data (amino acid sequence, molecular weight, and charge) for the proteins in cell
@@ -197,7 +227,7 @@ class RandomSeqGen(object):
 '''
 RandomSeqGen = RandomSeqGen()
 NUM_GENES = 10
-(genes, rna, proteins) = RandomSeqGen.gen_species_types(NUM_GENES)
+(genes, rnas, proteins) = RandomSeqGen.gen_species_types(NUM_GENES)
 OUT_FILE = 'protein_data.csv'
 RandomSeqGen.output_sequences(proteins, OUT_FILE)
 '''
--- tests/test_random_polymer.py
+++ tests/test_random_polymer.py
@@ -11,6 +11,7 @@ import numpy as np
 from random_wc_model_generator.random_polymer import RandomSeqGen
 
 NUM_GENES=10
+NUM_RUNS=100
 
 class TestGenerateSeq(unittest.TestCase):
     
@@ -20,38 +21,37 @@ class TestGenerateSeq(unittest.TestCase):
     def test_length(self):
         ''' Compare length of protein (in amino acids) with length assigned by randomized protein length list
         '''
+        prot_lengths = self.rsg.prot_len(num_genes=NUM_GENES)
+        genome = self.rsg.make_genome(prot_lengths)
+        genes = self.rsg.make_genes(genome)
+        rnas = self.rsg.make_mrnas(genes)
+        proteins = self.rsg.make_proteins(rnas)
         
-        prot_length = self.rsg.prot_len(num_genes=NUM_GENES)
-        rna = self.rsg.make_rna(NUM_GENES, prot_length)
-        proteins = self.rsg.make_proteins(rna)
-        
-        for i in range(len(rna)):
+        for i in range(len(rnas)):
             # account for lack of stop codon amino acid with +1
-            self.assertEqual(len(proteins[0][i])+1, prot_length[i])
+            self.assertEqual(len(proteins[0][i])+1, prot_lengths[i])
         
     def test_base_stats(self):
         ''' Check abundance of each base in gene nucleotide sequence
-        '''
+        '''        
+        base_abundance = np.zeros((NUM_RUNS,4))
+        for i in range(NUM_RUNS):
+            prot_length = self.rsg.prot_len(num_genes=NUM_GENES)
+            genome = self.rsg.make_genome(prot_length)
+
+            base_abundance[i][0] = genome.count('A')/len(genome)
+            base_abundance[i][1] = genome.count('C')/len(genome)
+            base_abundance[i][2] = genome.count('T')/len(genome)
+            base_abundance[i][3] = genome.count('G')/len(genome)
         
-        prot_length = self.rsg.prot_len(num_genes=NUM_GENES)
-        rna = self.rsg.make_rna(NUM_GENES, prot_length)
-        genes = self.rsg.make_genes(rna)
-        
-        base_abundance = np.zeros((len(genes),4))
-        for i in range(len(genes)):
-            base_abundance[i][0] = genes[i].count('A')/len(genes[i])
-            base_abundance[i][1] = genes[i].count('C')/len(genes[i])
-            base_abundance[i][2] = genes[i].count('T')/len(genes[i])
-            base_abundance[i][3] = genes[i].count('G')/len(genes[i])
         for j in range(0,4):
             np.testing.assert_allclose(np.mean(base_abundance[:][j]), 0.25, rtol = 0.2)
 
     def test_prot_data_reading(self):
         ''' Check valid values are being read and stored from protein data file
         '''
-        protein = self.rsg.prot_seq(length=100)
-        aa, mw, charge = self.rsg.prot_data(protein)
-        self.assertGreater(mw,0)
-        self.assertIsInstance(charge, int)
-
-    
+        (genes, rnas, proteins) = self.rsg.gen_species_types(NUM_GENES)
+        for i in range(NUM_GENES):
+            aa, mw, charge = self.rsg.prot_data(rnas[i])
+            self.assertGreater(mw,0)
+            self.assertIsInstance(charge, int)
