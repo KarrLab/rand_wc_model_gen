5aa49982986acc2b299926504207fd35fc5629cf
Jonathan Karr <jonrkarr@gmail.com>
2018-01-25 00:49:01 -0500

    debugging test_base_stats: converting int to float so that division returns a float rather than an int

--- tests/test_random_polymer.py
+++ tests/test_random_polymer.py
@@ -10,11 +10,12 @@ import numpy as np
 
 from random_wc_model_generator.random_polymer import RandomSeqGen
 
-NUM_GENES=10
-NUM_RUNS=100
+NUM_GENES = 10
+NUM_RUNS = 100
+
 
 class TestGenerateSeq(unittest.TestCase):
-    
+
     def setUp(self):
         self.rsg = RandomSeqGen()
 
@@ -26,26 +27,25 @@ class TestGenerateSeq(unittest.TestCase):
         genes = self.rsg.make_genes(genome)
         rnas = self.rsg.make_mrnas(genes)
         proteins = self.rsg.make_proteins(rnas)
-        
+
         for i in range(len(rnas)):
             # account for lack of stop codon amino acid with +1
             self.assertEqual(len(proteins[0][i])+1, prot_lengths[i])
-        
+
     def test_base_stats(self):
         ''' Check abundance of each base in gene nucleotide sequence
-        '''        
-        base_abundance = np.zeros((NUM_RUNS,4))
+        '''
+        base_abundance = np.zeros((NUM_RUNS, 4))
         for i in range(NUM_RUNS):
             prot_length = self.rsg.prot_len(num_genes=NUM_GENES)
             genome = self.rsg.make_genome(prot_length)
 
-            base_abundance[i][0] = genome.count('A')/len(genome)
-            base_abundance[i][1] = genome.count('C')/len(genome)
-            base_abundance[i][2] = genome.count('T')/len(genome)
-            base_abundance[i][3] = genome.count('G')/len(genome)
-        
-        for j in range(0,4):
-            np.testing.assert_allclose(np.mean(base_abundance[:][j]), 0.25, rtol = 0.2)
+            base_abundance[i][0] = float(genome.count('A')) / len(genome)
+            base_abundance[i][1] = float(genome.count('C')) / len(genome)
+            base_abundance[i][2] = float(genome.count('T')) / len(genome)
+            base_abundance[i][3] = float(genome.count('G')) / len(genome)
+
+        np.testing.assert_allclose(np.mean(base_abundance, 0), 0.25, rtol=0.1)
 
     def test_rna_transcript(self):
         ''' Check genome is properly transcripted into RNA
@@ -53,7 +53,7 @@ class TestGenerateSeq(unittest.TestCase):
         prot_length = self.rsg.prot_len(num_genes=NUM_GENES)
         genome = self.rsg.make_genome(prot_length)
         wc_rna = self.rsg.rna_transcript(genome)
-        
+
         gen_base = {}
         for nuc in RandomSeqGen.DNA_NUCLEOTIDES:
             gen_base[nuc] = genome.count(nuc)
@@ -65,7 +65,7 @@ class TestGenerateSeq(unittest.TestCase):
         (genes, rnas, proteins) = self.rsg.gen_species_types(NUM_GENES)
         for i in range(NUM_GENES):
             aa, mw, charge = self.rsg.prot_data(rnas[i])
-            self.assertGreater(mw,0)
+            self.assertGreater(mw, 0)
             self.assertIsInstance(charge, int)
 
     def test_output(self):
@@ -74,6 +74,6 @@ class TestGenerateSeq(unittest.TestCase):
         (genes, rnas, proteins) = self.rsg.gen_species_types(NUM_GENES)
         prot_data_string = self.rsg.output_prot_data(proteins)
         self.assertEqual(len(prot_data_string), NUM_GENES)
-        
+
         for gene in prot_data_string:
             self.assertIsInstance(gene, str)
