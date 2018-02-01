638bec5863096ee61b5ff2cf8e8590db088aac22
Arthur Goldberg <artgoldberg@gmail.com>
2018-02-01 14:50:13 -0500

    provide examples to Ashwin (finally!) and start testing enrich_polymers.py

---
+++ examples/find_base_models.py
@@ -0,0 +1,27 @@
+import os
+
+from wc_lang.core import Compartment, Reaction, SpeciesType, Species
+from wc_lang.io import Reader, Writer
+from obj_model.core import Validator
+
+model_core = os.path.join(os.path.dirname(__file__), '../random_wc_model_generator/data/fixtures', 'model_core.xlsx')
+model = Reader().run(model_core)
+
+AspAsp = model.get_component('species_type', 'AspAsp')
+# AspAsp.pprint(max_depth=0)
+
+ASP_c = list(filter(lambda s: s.id()=='ASP[c]', model.get_species()))
+if len(ASP_c) == 1:
+    # ASP_c[0].pprint(max_depth=1)
+    pass
+
+species_type_list = [SpeciesType(id="species_{}".format(i), model=model) for i in range(3)] 
+comp = Compartment(id='comp')
+species_list = [Species(species_type=st, compartment=comp) for st in species_type_list]
+rxn_1 = Reaction(id='test1')
+rxn_2 = Reaction(id='test2')
+rxn_1.participants.create(species=species_list[0], coefficient=1)
+rxn_2.participants.create(species=species_list[0], coefficient=1)
+
+error = Validator().run(rxn_1.participants + rxn_2.participants)
+print(error)
---
+++ tests/test_enrich_polymers.py
@@ -0,0 +1,18 @@
+"""
+:Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
+:Date: 2018-02-01
+:Copyright: 2016-2018, Karr Lab
+:License: MIT
+"""
+
+import unittest
+
+from random_wc_model_generator.enrich_polymers import Enrich_Polymers
+
+class TestEventQueue(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_enrich_polymers(self):
+        pass
