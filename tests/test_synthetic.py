cfe94e4a7724f54831a08103b905b05a124fa701
Bilal Shaikh <bilalshaikh42@gmail.com>
2018-06-13 14:59:45 -0400

    Implemented setup method

--- tests/test_synthetic.py
+++ tests/test_synthetic.py
@@ -1,5 +1,5 @@
 """ Test the generation of a random chromosome and accompanying mRNA and protein sequences
-: Author: Bilal Shaikh <bilal.shaikh@columbia.edu>
+:Author: Bilal Shaikh <bilal.shaikh@columbia.edu>
 :Author: Ashwin Srinivasan <ashwins@mit.edu>
 :Date: 2018-06-13
 :Copyright: 2018, Karr Lab
@@ -7,15 +7,21 @@
 """
 import unittest
 import numpy as np
+rand_wc_model_gen.Syntheticn
 
-from rand_wc_model_gen.Synthetic import create_chromosome, create_rnas_proteins
+
+GEN_LEN = 20
+INTER_LEN = 20
+GEN_NUM = 20
+TRANSLATION_TABLE = 1
 
 
 class TestSynthetic(unittest.TestCase):
     def setUp(self):
-        pass
+        synthetic = Synthetic()
+
     def tearDown(self):
         pass
-    
-        
 
+    def test_length(self):
+        synthetic.generate(GEN_LEN, INTER_LEN, GEN_NUM, TRANSLATION_TABLE)
