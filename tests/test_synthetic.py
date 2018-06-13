5d3efa2939b09058fb50461224b3aa9313b770bd
Bilal Shaikh <bilalshaikh42@gmail.com>
2018-06-13 14:38:43 -0400

    Created unit test for synthetic.py

---
+++ tests/test_synthetic.py
@@ -0,0 +1,12 @@
+""" Test the generation of a random chromosome and accompanying mRNA and protein sequences
+: Author: Bilal Shaikh <bilal.shaikh@columbia.edu>
+:Author: Ashwin Srinivasan <ashwins@mit.edu>
+:Date: 2018-06-13
+:Copyright: 2018, Karr Lab
+:License: MIT
+"""
+import unittest
+import numpy as np
+
+from rand_wc_model_gen.Synthetic import create_chromosome, create_rnas_proteins
+
