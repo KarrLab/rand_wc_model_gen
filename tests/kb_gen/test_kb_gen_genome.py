5dca7adbb665bc17131d7e02b6f5a1d96df4c0ca
Bilal Shaikh <bilalshaikh42@gmail.com>
2018-06-14 12:02:21 -0400

    created set up method to make test method cleaner

--- tests/kb_gen/test_kb_gen_genome.py
+++ tests/kb_gen/test_kb_gen_genome.py
@@ -5,32 +5,69 @@
 :Copyright: 2018, Karr Lab
 :License: MIT
 """
+import math
 import wc_kb
+import wc_kb_gen
 import unittest
 import numpy as np
 from rand_wc_model_gen.kb_gen import genome
+from Bio.Seq import Seq
 
-GEN_LEN = 20
-INTER_LEN = 20
+GEN_LEN = 10
+INTER_LEN = 10
 GEN_NUM = 20
 TRANSLATION_TABLE = 1
 
 
-class TestSynthetic(unittest.TestCase):
-    def setUp(self):
-        kb = wc_kb.KnowledgeBase()
-        kb.cell = wc_kb.Cell()
+class TestGenomeGenerator(unittest.TestCase):
 
+    def setUp(self):
+        # The knowledge base that is needed for the KBComponentGenerator
+        kb = wc_kb.KnowledgeBase()
+        cell = kb.cell = wc_kb.Cell()
+
+        # Creates the GenomeGenerator object and sets the parameters as given
         self.gen = genome.GenomeGenerator(kb)
+        self.gen.options = {'gen_len': GEN_LEN, 'inter_len': INTER_LEN,
+                            'gen_num': GEN_NUM, 'translation_table': TRANSLATION_TABLE}
+
+        # Generates the sequences
+        self.gen.gen_components()
+
+        # the gene sequence that was generated
+        self.seq = self.gen.knowledge_base.cell.species_types[0].get_seq()
+        # represented as a string
+        self.seq_str = str(self.seq)
+
+        # a list of tuples containing the starting and ending indices of each gene
+        self.indexlist = self.gen.indexList
+
+        # generates a list of the genes and of the intergenic regions
+        self.genes = []
+        self.intergenes = []
+
+        lastend = None
+
+        for tuple in self.indexlist:
+            self.genes.append(self.seq_str[tuple[0]:tuple[1]+1])
+
+            if lastend:
+                self.intergenes.append(self.seq_str[lastend:tuple[0]])
+            lastend = tuple[1]
+
+    def test_run(self):
+        self.assertIsInstance(self.seq, Seq)
+
+    def test_length(self):
+
+        # Tests that the average length of the genes + intergenenic sequences is within 3 standard deviations of the expected.
+        # TODO: check if this is an appropriate test, change if not
+        self.assertAlmostEqual(
+            len(self.seq)/GEN_NUM, (GEN_LEN+INTER_LEN)*3, delta=5*math.sqrt((INTER_LEN/GEN_NUM+GEN_LEN/GEN_NUM)))
 
     def tearDown(self):
         pass
 
-    def test_run(self):
-        self.gen.gen_genome(GEN_LEN, INTER_LEN, GEN_NUM, TRANSLATION_TABLE)
 
-    def test_length(self):
-        self.gen.gen_genome(GEN_LEN, INTER_LEN, GEN_NUM, TRANSLATION_TABLE)
-
-    def multiple_models(self):
-        pass
+if __name__ == '__main__':
+    unittest.main()
