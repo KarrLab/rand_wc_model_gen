22d08dc59b3de966a87542326784539dd8412106
Jonathan Karr <jonrkarr@gmail.com>
2018-06-14 15:14:50 -0400

    refactoring command line options to config file, starting analysis command

--- tests/test_main.py
+++ tests/test_main.py
@@ -22,6 +22,36 @@ class CliTestCase(unittest.TestCase):
     def setUp(self):
         self.temp_dir = tempfile.mkdtemp()
 
+        self.kb_core_path = os.path.join(self.temp_dir, 'kb', 'core', 'core.xlsx')
+        self.kb_seq_path = os.path.join(self.temp_dir, 'kb', 'seq', 'seq.fna')
+        self.model_path = os.path.join(self.temp_dir, 'model', 'model.xlsx')
+        self.sim_results_path = os.path.join(self.temp_dir, 'sim_results')
+        self.config_path = os.path.join(self.temp_dir, 'rand_wc_model_gen.cfg')
+
+        # write configuration file
+        with open(self.config_path, 'w') as file:
+            file.write('[rand_wc_model_gen]\n')
+            file.write('    [[kb_gen]]\n')
+            file.write('        set_repo_metadata_from_path = false\n')
+            file.write('        [[[component]]]\n')
+            file.write('            [[[[ChromosomesGenesTusGenerator]]]]\n')
+            file.write('                num_chromosomes = 1\n')
+            file.write('                mean_num_genes = 100\n')
+            file.write('                mean_gene_len = 10\n')
+            file.write('    [[kb]]\n')
+            file.write('        [[[path]]]\n')
+            file.write('            core = {}\n'.format(self.kb_core_path))
+            file.write('            seq = {}\n'.format(self.kb_seq_path))
+            file.write('    [[model_gen]]\n')
+            file.write('        set_repo_metadata_from_path = false\n')
+            file.write('    [[model]]\n')
+            file.write('        path = {}\n'.format(self.model_path))
+            file.write('    [[sim]]\n')
+            file.write('        end_time = 10\n')
+            file.write('    [[sim_results]]\n')
+            file.write('        checkpoint_period = 1\n')
+            file.write('        path = {}\n'.format(self.sim_results_path))
+
     def tearDown(self):
         shutil.rmtree(self.temp_dir)
 
@@ -52,59 +82,29 @@ class CliTestCase(unittest.TestCase):
             self.assertEqual(stdout.getvalue().strip(), rand_wc_model_gen.__version__)
             self.assertEqual(stderr.getvalue(), '')
 
-    def test_gen_model(self):
-        kb_core_path = os.path.join(self.temp_dir, 'kb.xlsx')
-        kb_seq_path = os.path.join(self.temp_dir, 'kb.fna')
-        model_path = os.path.join(self.temp_dir, 'model.xlsx')
-        config_path = os.path.join(self.temp_dir, 'rand_wc_model_gen.cfg')
-
-        # write configuration file
-        with open(config_path, 'w') as file:
-            file.write('[rand_wc_model_gen]\n')
-            file.write('    [[kb_gen]]\n')
-            file.write('        [[[component]]]\n')
-            file.write('            [[[[ChromosomesGenesTusGenerator]]]]\n')
-            file.write('                num_chromosomes = 1\n')
-            file.write('                mean_num_genes = 100\n')
-            file.write('                mean_gene_len = 10\n')
-
+    def test_generate(self):
         # generate model
-        with __main__.App(argv=['gen', kb_core_path, kb_seq_path, model_path, '--config-path', config_path]) as app:
+        with __main__.App(argv=['generate', '--config-path', self.config_path]) as app:
             app.run()
 
-        self.assertTrue(os.path.isfile(kb_core_path))
-        self.assertTrue(os.path.isfile(kb_seq_path))
-        self.assertTrue(os.path.isfile(model_path))
-        kb = wc_kb.io.Reader().run(kb_core_path, kb_seq_path)
-        model = wc_lang.io.Reader().run(model_path)
+        self.assertTrue(os.path.isfile(self.kb_core_path))
+        self.assertTrue(os.path.isfile(self.kb_seq_path))
+        self.assertTrue(os.path.isfile(self.model_path))
+        kb = wc_kb.io.Reader().run(self.kb_core_path, self.kb_seq_path)
+        model = wc_lang.io.Reader().run(self.model_path)
 
-    def test_sim_model(self):
-        kb_core_path = os.path.join(self.temp_dir, 'kb.xlsx')
-        kb_seq_path = os.path.join(self.temp_dir, 'kb.fna')
-        model_path = os.path.join(self.temp_dir, 'model.xlsx')
-        config_path = os.path.join(self.temp_dir, 'rand_wc_model_gen.cfg')
-
-        # write configuration file
-        with open(config_path, 'w') as file:
-            file.write('[rand_wc_model_gen]\n')
-            file.write('    [[kb_gen]]\n')
-            file.write('        [[[component]]]\n')
-            file.write('            [[[[ChromosomesGenesTusGenerator]]]]\n')
-            file.write('                num_chromosomes = 1\n')
-            file.write('                mean_num_genes = 100\n')
-            file.write('                mean_gene_len = 10\n')
-            file.write('    [[sim]]\n')
-            file.write('        end_time = 10\n')
-            file.write('        checkpoint_period = 1\n')
-
+    def test_simulate(self):
         # generate model
-        with __main__.App(argv=['gen', kb_core_path, kb_seq_path, model_path, '--config-path', config_path]) as app:
+        with __main__.App(argv=['generate', '--config-path', self.config_path]) as app:
             app.run()
 
         # simulate model
-        sim_results_path = os.path.join(self.temp_dir, 'sim_results')
-        with __main__.App(argv=['sim', model_path, sim_results_path, '--config-path', config_path]) as app:
+        with __main__.App(argv=['simulate', '--config-path', self.config_path]) as app:
             app.run()
 
         # assert
         self.assertTrue(os.path.isfile(os.path.join(app.results['sim_results_path'], 'run_results.h5')))
+
+    @unittest.skip('Implement')
+    def test_analyze(self):
+        pass
