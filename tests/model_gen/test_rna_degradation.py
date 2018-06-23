e3ac4f89c72ee78be174ee8e521f9ed922584683
Bilal Shaikh <bilalshaikh42@gmail.com>
2018-06-23 10:43:42 -0400

    changed to new module

--- tests/model_gen/test_rna_degradation.py
+++ tests/model_gen/test_rna_degradation.py
@@ -23,7 +23,7 @@ class RnaDegradationSubmodelGeneratorTestCase(unittest.TestCase):
                     'mean_volume': 1e-15,
                     'mean_doubling_time': 1000.,
                 },
-                'ChromosomesGenesTusGenerator': {
+                'GenomeGenerator': {
                     'num_chromosomes': 1,
                     'mean_num_genes': 100.,
                     'mean_gene_len': 100.,
@@ -40,7 +40,8 @@ class RnaDegradationSubmodelGeneratorTestCase(unittest.TestCase):
         rnas = cell.species_types.get(__type=wc_kb.RnaSpeciesType)
 
         model = wc_lang.Model()
-        gen = rna_degradation.RnaDegradationSubmodelGenerator(kb, model, options={})
+        gen = rna_degradation.RnaDegradationSubmodelGenerator(
+            kb, model, options={})
         gen.run()
 
         submodel = model.submodels.get_one(id='rna_degradation')
@@ -52,24 +53,33 @@ class RnaDegradationSubmodelGeneratorTestCase(unittest.TestCase):
         # check species types and species generated
         amp = model.species_types.get_one(id='amp')
         amp_cytosol = amp.species.get_one(compartment=cytosol)
-        self.assertEqual(amp_cytosol.concentration.units, wc_lang.ConcentrationUnit.M)
+        self.assertEqual(amp_cytosol.concentration.units,
+                         wc_lang.ConcentrationUnit.M)
 
         concs = []
         for species_type in model.species_types:
             if species_type.id.startswith('rna_'):
                 species = species_type.species.get_one(compartment=cytosol)
                 concs.append(species.concentration.value)
-                self.assertEqual(species.concentration.units, wc_lang.ConcentrationUnit.M)
-        numpy.testing.assert_almost_equal(numpy.mean(concs), 10. / scipy.constants.Avogadro / 1e-15, decimal=2)
+                self.assertEqual(species.concentration.units,
+                                 wc_lang.ConcentrationUnit.M)
+        numpy.testing.assert_almost_equal(numpy.mean(
+            concs), 10. / scipy.constants.Avogadro / 1e-15, decimal=2)
 
         # check reactions generated
         self.assertEqual(len(submodel.reactions), len(rnas))
-        amp = model.species_types.get_one(id='amp').species.get_one(compartment=cytosol)
-        cmp = model.species_types.get_one(id='cmp').species.get_one(compartment=cytosol)
-        gmp = model.species_types.get_one(id='gmp').species.get_one(compartment=cytosol)
-        ump = model.species_types.get_one(id='ump').species.get_one(compartment=cytosol)
-        h2o = model.species_types.get_one(id='h2o').species.get_one(compartment=cytosol)
-        h = model.species_types.get_one(id='h').species.get_one(compartment=cytosol)
+        amp = model.species_types.get_one(
+            id='amp').species.get_one(compartment=cytosol)
+        cmp = model.species_types.get_one(
+            id='cmp').species.get_one(compartment=cytosol)
+        gmp = model.species_types.get_one(
+            id='gmp').species.get_one(compartment=cytosol)
+        ump = model.species_types.get_one(
+            id='ump').species.get_one(compartment=cytosol)
+        h2o = model.species_types.get_one(
+            id='h2o').species.get_one(compartment=cytosol)
+        h = model.species_types.get_one(
+            id='h').species.get_one(compartment=cytosol)
         self.assertEqual(
             + submodel.reactions[0].participants.get_one(species=amp).coefficient
             + submodel.reactions[0].participants.get_one(species=cmp).coefficient
@@ -88,10 +98,13 @@ class RnaDegradationSubmodelGeneratorTestCase(unittest.TestCase):
             self.assertEqual(len(rxn.rate_laws), 1)
             rl = rxn.rate_laws[0]
             self.assertEqual(rl.direction.name, 'forward')
-            self.assertEqual(rl.equation.expression, 'k_cat * {0}[c] / (k_m + {0}[c])'.format(rna.id))
-            self.assertEqual(rl.equation.modifiers, [rxn.participants[0].species])
+            self.assertEqual(rl.equation.expression,
+                             'k_cat * {0}[c] / (k_m + {0}[c])'.format(rna.id))
+            self.assertEqual(rl.equation.modifiers, [
+                             rxn.participants[0].species])
             self.assertEqual(rl.equation.parameters, [])
             self.assertEqual(rl.k_m, rna.concentration)
 
         k_cats = [rxn.rate_laws[0].k_cat for rxn in submodel.reactions]
-        numpy.testing.assert_almost_equal(numpy.mean(k_cats), 2. * numpy.log(2.) * (1. / 120.), decimal=2)
+        numpy.testing.assert_almost_equal(numpy.mean(
+            k_cats), 2. * numpy.log(2.) * (1. / 120.), decimal=2)
