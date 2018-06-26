cf656ebed248bea4643955621ee824e713c78122
Ashwin Srinivasan <ashwins9999@gmail.com>
2018-06-26 16:42:40 -0400

    Changed test_config to use GenomeGenerator

--- tests/test_config.py
+++ tests/test_config.py
@@ -25,32 +25,32 @@ class Test(unittest.TestCase):
 
     def test_get_config(self):
         config = rand_wc_model_gen.config.get_config()
-        self.assertEqual(config['rand_wc_model_gen']['kb_gen']['component']['ChromosomesGenesTusGenerator']['num_chromosomes'], 1)
-        self.assertEqual(config['rand_wc_model_gen']['kb_gen']['component']['ChromosomesGenesTusGenerator']['mean_gc_frac'], 0.5)
+        self.assertEqual(config['rand_wc_model_gen']['kb_gen']['component']['GenomeGenerator']['num_chromosomes'], 1)
+        self.assertEqual(config['rand_wc_model_gen']['kb_gen']['component']['GenomeGenerator']['mean_gc_frac'], 0.5)
 
     def test_get_config_extra_path(self):
         with open(self.extra_path, 'w') as file:
             file.write('[rand_wc_model_gen]\n')
             file.write('    [[kb_gen]]\n')
             file.write('        [[[component]]]\n')
-            file.write('            [[[[ChromosomesGenesTusGenerator]]]]\n')
+            file.write('            [[[[GenomeGenerator]]]]\n')
             file.write('                num_chromosomes = 5\n')
 
         config = rand_wc_model_gen.config.get_config(extra_path=self.extra_path)
-        self.assertEqual(config['rand_wc_model_gen']['kb_gen']['component']['ChromosomesGenesTusGenerator']['num_chromosomes'], 5)
-        self.assertEqual(config['rand_wc_model_gen']['kb_gen']['component']['ChromosomesGenesTusGenerator']['mean_gc_frac'], 0.5)
+        self.assertEqual(config['rand_wc_model_gen']['kb_gen']['component']['GenomeGenerator']['num_chromosomes'], 5)
+        self.assertEqual(config['rand_wc_model_gen']['kb_gen']['component']['GenomeGenerator']['mean_gc_frac'], 0.5)
 
     def test_get_config_extra_vals(self):
         config = rand_wc_model_gen.config.get_config(extra_vals={
             'rand_wc_model_gen': {
                 'kb_gen': {
                     'component': {
-                        'ChromosomesGenesTusGenerator': {
+                        'GenomeGenerator': {
                             'num_chromosomes': 10,
                         },
                     },
                 }
             }
         })
-        self.assertEqual(config['rand_wc_model_gen']['kb_gen']['component']['ChromosomesGenesTusGenerator']['num_chromosomes'], 10)
-        self.assertEqual(config['rand_wc_model_gen']['kb_gen']['component']['ChromosomesGenesTusGenerator']['mean_gc_frac'], 0.5)
+        self.assertEqual(config['rand_wc_model_gen']['kb_gen']['component']['GenomeGenerator']['num_chromosomes'], 10)
+        self.assertEqual(config['rand_wc_model_gen']['kb_gen']['component']['GenomeGenerator']['mean_gc_frac'], 0.5)
