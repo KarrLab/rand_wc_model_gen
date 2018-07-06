01cd1d160ff281437a644927b9b0ce6c698c8bf6
Bilal Shaikh <bilalshaikh42@gmail.com>
2018-07-06 15:28:06 -0400

    Removed files moved to wc_model_gen

--- rand_wc_model_gen/kb_gen/__init__.py
+++
@@ -1,5 +0,0 @@
-from .core import KbGenerator
-from .genome import GenomeGenerator
-from .metabolites import MetabolitesGenerator
-from .properties import PropertiesGenerator
-from .rna import RnaGenerator
