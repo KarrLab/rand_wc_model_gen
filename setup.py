5cc2c8acdec107bade19163467664253918ac912
Jonathan Karr <jonrkarr@gmail.com>
2019-08-16 10:27:42 -0400

    changing contact email to info@karrlab.org

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
     url="https://github.com/KarrLab/" + name,
     download_url='https://github.com/KarrLab/' + name,
     author="Karr Lab",
-    author_email="karr@mssm.com",
+    author_email="info@karrlab.org",
     license="MIT",
     keywords='whole-cell systems biology',
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
